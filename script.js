96037b5d8c5059f8b30544baab6de218b3456617
Emma30h <emma30h@gmail.com>
2022-06-09 22:14:39 -0300

    actualizacion V

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+//MENU-HAMBURGUESA
 let menu = document.getElementById("menu");
 let nav = document.getElementById("nav");
 
 menu.addEventListener("click", ()=>{
     nav.classList.toggle("nav-translate"); 
+    if(nav.classList.contains("nav-translate")){
+        menu.style.backgroundImage = "url('/img/icons/x.svg')";
+    }
+    else if(nav.classList.contains("nav")){
+        menu.style.backgroundImage = "url('/img/icons/menu-hambuerguesa.svg')";
+    }
 });
 
 window.addEventListener("click", (e)=>{
     // console.log(e.target);
     if(nav.classList.contains("nav-translate") && e.target != nav && e.target != menu){
         nav.classList.toggle("nav-translate");
+        if(nav.classList.contains("nav-translate")){
+            menu.style.backgroundImage = "url('/img/icons/x.svg')";
+        }
+        else if(nav.classList.contains("nav")){
+            menu.style.backgroundImage = "url('/img/icons/menu-hambuerguesa.svg')";
+        }
     }
 });
 
+//LOADER
 window.addEventListener("load", ()=>{
     console.log("Entre");
     document.getElementById("loader").classList.toggle("loader2");
